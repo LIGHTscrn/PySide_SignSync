6d68c09800c58936e5ae99ba0a44586b21133b62
LIGHTscrn <pranavprasad.r500@gmail.com>
2026-08-24 20:06:09 +0530

    Included youtubePlayer

--- Main_Window.py
+++ Main_Window.py
@@ -1,19 +1,42 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QLineEdit, QPushButton, QWidget
 
+from PySide6.QtWebEngineWidgets import QWebEngineView
+
 
 class Main_Window(QMainWindow):
 
     def __init__(self):
-        super.__init__()
+        super().__init__()
 
         #Creating an Blank Middle Man Widget
-        TopWidget = QWidget()
+        topWidget = QWidget()
         layout = QVBoxLayout()
+
 
         #Creating an Input Widget
         url_input = QLineEdit()
         play_button = QPushButton("Go")
 
+        #Creating a WebEngineView
+        youtubePlayer = QWebEngineView()
+
+        youtubeIframe = '''
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/oLxFqpUbaAE?si=vixAthV6v4bQBH20" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen ></iframe>
+        '''
+        youtubePlayer.setHtml(youtubeIframe)
+        
+        layout.addWidget(url_input)
+        layout.addWidget(play_button)
+        layout.addWidget(youtubePlayer)
+
+        topWidget.setLayout(layout)
+        self.setCentralWidget(topWidget)
+
+
+app = QApplication()
+window = Main_Window()
+window.show()
+app.exec()
 
 
 
